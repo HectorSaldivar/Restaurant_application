aef80109283fdffb03ed4f9a98375be31280b749
MManuel <manuelistenango@gmail.com>
2022-07-31 10:14:51 -0700

    second commit

---
+++ src/components/Instagram.js
@@ -0,0 +1,25 @@
+import { FaInstagram } from "react-icons/fa";
+import React from 'react'
+
+
+function Instagram() {
+  return (
+<>
+    <div className="instagram"> 
+      
+      <span> <FaInstagram className="icon"/> Instagram</span>
+
+      <div className="widget" class="elfsight-app-19fda28e-ab20-4ea9-bbc6-d4984d2c5ebe"></div>
+      <div className="schedule">We’re open from<span className="scheduleTime"> 8:30am</span> to <span className="scheduleTime"> 3:00pm</span> everyday!!!</div>
+    </div>
+
+    <div>
+       <div>Need some help, send us a message </div>
+       <div>(604) 245-7855</div>
+    </div>
+
+</>
+  )
+}
+
+export default Instagram
