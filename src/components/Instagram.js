cf9006ffa973f374f7e04052dfdc341706d9c358
MManuel <manuelistenango@gmail.com>
2022-08-01 11:31:23 -0700

    7 commit we added some styles and still working mostly on that

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -7,17 +7,18 @@ function Instagram() {
 <>
     <div className="instagram"> 
       
-      <div>
+      <div classNAme="instaTitle">
       <span className="instagramTitle"> <FaInstagram className="icon"/> Instagram</span>
       </div>
 
-      <div className="widget" class="elfsight-app-19fda28e-ab20-4ea9-bbc6-d4984d2c5ebe"></div>
-      <div className="schedule">We’re open from<span className="scheduleTime"> 8:30am</span> to <span className="scheduleTime"> 3:00pm</span> everyday!!!</div>
+      <div className="instagramWidget" class="elfsight-app-19fda28e-ab20-4ea9-bbc6-d4984d2c5ebe"></div>
+      <div className="schedule">We’re open from <div><span className="scheduleTime"> 8:30am</span> to <span className="scheduleTime"> 3:00pm</span></div></div>
+
     </div>
 
-    <div>
-       <div>Need some help, send us a message </div>
-       <div>(604) 245-7855</div>
+    <div className="contact">
+       <div className="contactInfo">Need some help, send us a message </div>
+       <div className="contactNumber">(604) 245-7855</div>
     </div>
 
 </>
