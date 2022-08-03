bd860ccb5115e3288e265f32f9bef73f1dcdabcf
MManuel <manuelistenango@gmail.com>
2022-08-02 22:53:48 -0700

    new changes to the delivery component

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
 function Delivery() {
-  return (
-    <div className='delivery'>
-        <h1>Delivery</h1>
-        <div className='deliveryIcons'>
-            <h1>h</h1>
-            <h1>h</h1>
-            <h1>h</h1>
+    return (
+        <div className='delivery'>
+            <h1>Delivery</h1>
+            <div className='deliveryIcons'>
+                    <span className='firstLogo'>
+                        <a href='/'>Home</a>
+                    </span>
+                    <span className='secondLogo'>
+                        <a href='/'>Menu</a>
+                    </span>
+                    <span className='thirdLogo'>
+                        <a href='/'>
+                            <img src="https://cdn.iconscout.com/icon/free/png-256/uber-eats-1613370-1369417.png" alt="First slide"></img>
+                        </a>
+                    </span>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Delivery
+
