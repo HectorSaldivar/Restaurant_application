f39867c660b5e6724761b8366841d2a56402ca2e
MManuel <manuelistenango@gmail.com>
2022-08-05 09:11:02 -0700

    we added some new atyles, and also wrapping the menu images in anchor tags to make them work as a link, and also working in another screen for the menu dishes

---
+++ src/pages/dishesMenuScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import Navbar from '../components/Navbar'
+
+function dishesMenuScreen() {
+  return (
+    <>
+     <div>Hola</div>
+    </>
+  )
+}
+
+export default dishesMenuScreen
